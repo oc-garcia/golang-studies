e98f0b5327ba885887ed8cfadf43e4242ed8c6d4
oc-garcia <octaviogarcia1988@gmail.com>
2024-05-15 20:48:47 -0300

    Refactor main.go for better readability and consistency

---
+++ Api/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type User struct {
+	Id    int
+	Name  string
+	Email string
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/users", listUsersHandler)
+	http.ListenAndServe(":8080", mux)
+}
+
+func listUsersHandler(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("sqlite3", "users.db")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	users := []User{}
+	for rows.Next() {
+		var u User
+		if err := rows.Scan(&u.Id, &u.Name, &u.Email); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		users = append(users, u)
+	}
+
+	if err:= json.NewEncoder(w).Encode(users); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
--- go.mod
+++
@@ -1,3 +0,0 @@
-module home/oc-garcia/Documents/PersonalProjects/golang-studies
-
-go 1.22.3
